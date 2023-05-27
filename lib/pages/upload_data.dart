dcdf69e1fb16d74ce8cda2beaf9401dd59d3c6d6
raihanulhaque <raihanulhaque007@gmail.com>
2023-05-28 00:28:57 +0600

    Fixed Invalid QR issue

--- lib/pages/upload_data.dart
+++ lib/pages/upload_data.dart
@@ -9,68 +9,87 @@ class QRCodeResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final jsonMap = jsonDecode(qrCodeData);
-
-    final tableRows = <TableRow>[];
-    jsonMap.forEach((key, value) {
-      tableRows.add(TableRow(
-        children: [
-          TableCell(
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                key.toString(),
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          TableCell(
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(value.toString()),
-            ),
-          ),
-        ],
-      ));
-    });
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Submit Result'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      // body: Center(
-      //   child: Text(
-      //     qrCodeData,
-      //     style: TextStyle(fontSize: 24),
-      //   ),
-      // ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 40),
-          child: Column(
+    try {
+      final jsonMap = jsonDecode(qrCodeData);
+      if (jsonMap != null && jsonMap is Map) {
+        final tableRows = <TableRow>[];
+        jsonMap.forEach((key, value) {
+          tableRows.add(TableRow(
             children: [
-              Center(
-                child: Text(
-                  'Paitent Information',
-                  style: TextStyle(fontSize: 24),
+              TableCell(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    key.toString(),
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 20, left: 10, right: 10),
-                child: Table(
-                  border: TableBorder.all(),
-                  children: tableRows,
+              TableCell(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(value.toString()),
                 ),
               ),
             ],
+          ));
+        });
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Submit Result'),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new_rounded),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Column(
+                children: [
+                  const Center(
+                    child: Text(
+                      'Paitent Information',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20, left: 10, right: 10),
+                    child: Table(
+                      border: TableBorder.all(),
+                      children: tableRows,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      } else {
+        throw const FormatException('Invalid JSON data');
+      }
+    } catch (e) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Submit Result'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_new_rounded),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
-      ),
-    );
+        body: const SafeArea(
+          child: Center(
+            child: Text(
+              'Invalid QR Code',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      );
+    }
   }
 }
